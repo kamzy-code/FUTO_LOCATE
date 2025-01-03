ab0fe3b83f1ae0dd6f4aeef7b98de2dcdb820fd2
Kamzy <udochukwuchikezieloveday@gmail.com>
2025-01-03 12:38:40 +0100

    initial dahsboard activity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,4 +40,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation("org.osmdroid:osmdroid-android:6.1.14")
+
 }
---
+++ app/src/main/java/io/kamzy/futolocate/Dashboard.java
@@ -0,0 +1,35 @@
+package io.kamzy.futolocate;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+
+public class Dashboard extends AppCompatActivity {
+
+    MapView mapView = findViewById(R.id.mapView);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_dashboard);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        mapView.setTileSource(TileSourceFactory.MAPNIK);
+        mapView.getController().setZoom(15);
+        GeoPoint futoLocation = new GeoPoint(5.3792, 6.9974);
+        mapView.getController().setCenter(futoLocation);
+    }
+}
