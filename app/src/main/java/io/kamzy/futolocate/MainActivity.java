255aa20a5dabe3fc88032f61aba1d11d03af7067
Kamzy <udochukwuchikezieloveday@gmail.com>
2025-01-03 16:25:48 +0100

    add signup text button to login page

--- app/src/main/java/io/kamzy/futolocate/MainActivity.java
+++ app/src/main/java/io/kamzy/futolocate/MainActivity.java
@@ -1,6 +1,11 @@
 package io.kamzy.futolocate;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -10,16 +15,37 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    EditText email, password;
+    TextView forgot_password, signup;
+    Button login;
+    Context ctx;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        ctx = this;
+        email = findViewById(R.id.login_email_input);
+        password = findViewById(R.id.login_password_input);
+        forgot_password = findViewById(R.id.forgot_password);
+        signup = findViewById(R.id.Signup_text_button);
+        login = findViewById(R.id.login_button);
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ctx, Signup.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
